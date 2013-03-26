c3bc4cad9989d262f00c085464c970d79b780efc
tunde oni <tundeoni77@gmail.com>
2013-03-26 00:27:32 -0400

    new site

---
+++ public/javascripts/script.js
@@ -0,0 +1,22 @@
+if($.browser.mozilla||$.browser.opera){document.removeEventListener("DOMContentLoaded",$.ready,false);document.addEventListener("DOMContentLoaded",function(){$.ready()},false)}$.event.remove(window,"load",$.ready);$.event.add( window,"load",function(){$.ready()});$.extend({includeStates:{},include:function(url,callback,dependency){if(typeof callback!='function'&&!dependency){dependency=callback;callback=null}url=url.replace('\n','');$.includeStates[url]=false;var script=document.createElement('script');script.type='text/javascript';script.onload=function(){$.includeStates[url]=true;if(callback)callback.call(script)};script.onreadystatechange=function(){if(this.readyState!="complete"&&this.readyState!="loaded")return;$.includeStates[url]=true;if(callback)callback.call(script)};script.src=url;if(dependency){if(dependency.constructor!=Array)dependency=[dependency];setTimeout(function(){var valid=true;$.each(dependency,function(k,v){if(!v()){valid=false;return false}});if(valid)document.getElementsByTagName('head')[0].appendChild(script);else setTimeout(arguments.callee,10)},10)}else document.getElementsByTagName('head')[0].appendChild(script);return function(){return $.includeStates[url]}},readyOld:$.ready,ready:function(){if($.isReady) return;imReady=true;$.each($.includeStates,function(url,state){if(!state)return imReady=false});if(imReady){$.readyOld.apply($,arguments)}else{setTimeout(arguments.callee,10)}}});
+$.include('js/superfish.js')
+$.include('js/jquery.hoverIntent.minified.js')
+$.include('js/tms-0.4.1.js')
+$.include('js/jquery.easing.1.3.js')
+$(function(){	
+	if($('#form').length)$.include('js/forms.js');
+	if($('.lightbox-image').length)$.include('js/jquery.prettyPhoto.js');
+$('.slider')._TMS({
+			prevBu:'.prev',
+			nextBu:'.next',
+			pauseOnHover:true,
+			pagNums:false,
+			duration:800,
+			easing:'easeOutQuad',
+			preset:'Fade',
+			slideshow:7000,
+			pagination:'.pagination',
+			waitBannerAnimation:false,
+			banners:'fromLeft'
+			})
+});
